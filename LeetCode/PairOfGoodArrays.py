ed950bc20377b690f2bf79e1324c8ba59bce9a2e
Carlos <carloshenrique20051bb@gmail.com>
2025-05-03 18:46:34 -0300

    leetcode 1512

--- LeetCode/PairOfGoodArrays.py
+++ LeetCode/PairOfGoodArrays.py
@@ -6,9 +6,19 @@ class Solution(object):
 
         for num in nums:
             if num in hashMap:
-                counter = hashMap[num]
+                count += hashMap[num]
+                hashMap[num] += 1
+            else:
+                hashMap[num] = 1
+        
+        return count # tempo = O(n) e space = O(1)
 
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             count+=1
+        
+        # return count
 
-
-
-
+        # # 1,2,3,1,1,3 O(n²)
